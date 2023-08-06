5d144a6e8b076900207372aba515580320011b99
Ivan <doronin_ivan@mail.ru>
2023-08-06 18:33:32 +0300

    Added shooting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,29 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerShoot : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField]
     private GameObject _bulletPrefab;
 
+    [Header("Values")]
     [SerializeField]
     private float _bulletSpeed;
 
     [SerializeField]
+    private float _timeBetweenShots;
+    [SerializeField]
+    private float _lastFireTime;
+
+    [Header("Firing status")]
+    [SerializeField]
     private bool _fireContinuously;
+    [SerializeField]
+    private bool _fireSingle;
 
     void Update()
     {
-        if(_fireContinuously)
+        if(_fireContinuously || _fireSingle)
         {
-            FireBullet();
+            float timeSinceLastFire = Time.time - _lastFireTime;
+
+            if (timeSinceLastFire >= _timeBetweenShots)
+            {
+                FireBullet();
+
+                _lastFireTime = Time.time;
+                _fireSingle = false;
+            }
         }
     }
 
     private void FireBullet()
     {
-        GameObject bullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
+        GameObject bullet = Instantiate(_bulletPrefab, transform.position + transform.up * 1f, transform.rotation);
         Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
         rigidbody.velocity = _bulletSpeed * transform.up;
     }
@@ -32,5 +51,10 @@ public class PlayerShoot : MonoBehaviour
     private void OnFire(InputValue inputValue)
     {
         _fireContinuously = inputValue.isPressed;
+
+        if(inputValue.isPressed)
+        {
+            _fireSingle = true;
+        }
     }
 }
